ee82d057648721b6844e17a089a0cb9e7d40ac39
Johan Esneider Lucumi Palacios <johanesneiderlucumip@gmail.com>
2025-12-04 02:30:33 -0500

    Configurar DB con Railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,8 +6,8 @@ export const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   connectionLimit: 5,
 });
 
