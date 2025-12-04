a9453327985a6809ffa1ffa12b5e26c49e719034
Johan Esneider Lucumi Palacios <johanesneiderlucumip@gmail.com>
2025-12-04 04:39:44 -0500

    Configuracion backend y frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,21 @@ import cors from "cors";
 import router from "./routes/index.js";
 
 const app = express();
-app.use(cors());
+
+// 1. DEFINE EL DOMINIO DE TU FRONTEND EN NETLIFY
+// ¡ATENCIÓN!: Reemplaza ESTE valor con tu URL real de Netlify
+const allowedOrigin = "https://strong-choux-120ee2.netlify.app/";
+
+// 2. CONFIGURACIÓN DE CORS
+const corsOptions = {
+  origin: allowedOrigin,
+  // Puedes definir métodos si lo necesitas:
+  // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  // credentials: true // Útil si usas cookies de sesión
+};
+
+// 3. USA LA CONFIGURACIÓN ESPECÍFICA
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", router);
