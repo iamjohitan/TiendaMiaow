cefeb9b9a06e6650406527a2b283c91153ffa7d8
Johan Esneider Lucumi Palacios <johanesneiderlucumip@gmail.com>
2025-12-04 12:40:32 -0500

    Contacto funcional

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import contactoRoutes from "./contacto.routes.js";
-import productosRoutes from "./productos.routes.js";
 
 console.log("📌 Cargando rutas de contacto...");
 
 const router = Router();
 
 router.use("/contacto", contactoRoutes);
-router.use("/productos", productosRoutes);
 
 export default router;
